bfaf2878cf69eea44c953aa627864571e7d799be
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-05-05 22:31:46 -0300

    Imprimir numeros impares

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NumerosImpares.ConsoleApp;
+
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        // Iterar de 100 a 200
+        for (int numero = 100; numero <= 200; numero++)
+        {
+            // Verificar se o número é ímpar (o resto da divisão por 2 é diferente de 0)
+            if (numero % 2 != 0)
+            {
+                // Se for ímpar, imprimir o número
+                Console.WriteLine(numero);
+            }
+        }
+        Console.ReadLine(); 
+
+
+    }
+    }
+
+ 
