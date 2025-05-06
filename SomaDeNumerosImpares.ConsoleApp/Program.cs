fcbf05745dff5a1cd2bfe2e66d7ef182090ade40
Diego Toledo <Diego_toledo3010@hotmail.com>
2025-05-05 22:49:38 -0300

    Soma de numeros impares dividiveis por 3

---
+++ SomaDeNumerosImpares.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SomaDeNumerosImpares.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            for (int numero = 1; numero <= 500; numero++)
+            {
+                if (numero % 2 != 0)
+                {
+                    if(numero % 3 == 0)
+
+                        soma = soma + numero;
+                }
+
+            }
+
+            Console.WriteLine($"O resultado da soma é: "+ soma);
+            Console.ReadKey();
+        }
+    }
+}
